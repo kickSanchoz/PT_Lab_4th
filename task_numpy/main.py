afd806ab0bcb1394c4e0d22b935fcd8d66563138
sanchoz <alexvaa2010@mail.ru>
2022-11-24 01:29:21 +0300

    инициализация p, qT. Нахождение q, f, g

--- task_numpy/main.py
+++ task_numpy/main.py
@@ -1,15 +1,14 @@
 import numpy as np
 
+countN = 85
+
 
 def initMatrix():
-    N = 85
-
-    matrixM_diag = [i * 2 + 1 for i in range(1, N + 1)]
+    matrixM_diag = [i * 2 + 1 for i in range(1, countN + 1)]
     matrixM = np.diag(np.array(matrixM_diag))
     matrixM[(matrixM == 0)] = 2
-    print(matrixM)
 
-    matrixN = np.zeros((N, N))
+    matrixN = np.zeros((countN, countN))
     matrixN_row_even = [4, 4, 5]
     matrixN_row_odd = [4, 7]
     for indexR, row in enumerate(matrixN):
@@ -23,11 +22,82 @@ def initMatrix():
             else:
                 pos = indexC % len(matrixN_row_odd)
                 matrixN[indexR, indexC] = matrixN_row_odd[pos]
-    print(matrixN)
+    return matrixM, matrixN
+
+
+def pSet(arr, i):
+    if i < countN:
+        arr = np.append(arr, 1 + np.random.normal(2, 4))
+        i += 1
+        arr = pSet(arr, i)
+    return arr
+
+
+def qSet(arr, j, matrixN):
+    if j < countN:
+        arr = np.append(arr, np.multiply(matrixN[24, j], matrixN[71, j]))
+        j += 1
+        arr = qSet(arr, j, matrixN)
+    return arr
+
+
+def findF(q, qT, p):
+    multQ = np.dot(q, qT)
+
+    f = np.linalg.det(multQ)
+
+    result = np.dot(f, p)
+    return result
+
+
+def sumG(matrixM, matrixN, multQ, i, arr):
+    if i < countN:
+        _sum = matrixM[i, i] - matrixN[i, i] + multQ
+
+        arr = np.append(arr, _sum)
+        i += 1
+        arr = sumG(matrixM, matrixN, multQ, i, arr)
+    return arr
+
+
+def findG(matrixM, matrixN, q, qT, p):
+    multQ = np.dot(qT, q)
+
+    _set = sumG(matrixM, matrixN, multQ, 0, np.array([]))
+
+    result = np.linalg.norm(_set, 2)
+
+    return result
+
+
+def initialConditions():
+    M, N = initMatrix()
+    print("M = {}".format(M), "\n")
+    print("N = {}".format(N), "\n")
+
+    p = pSet(arr=np.array([]), i=0)
+    print("p = {}".format(p), "\n")
+
+    qT = qSet(arr=np.array([]), j=0, matrixN=N)[np.newaxis]
+    print("qT = {}".format(qT), "\n")
+    q = qT.T
+    print("q = {}".format(q), "\n")
+
+    resultF = findF(q, qT, p)
+    print("f = {}".format(resultF), "\n")
+
+    resultG = findG(
+        matrixM=M,
+        matrixN=N,
+        q=q,
+        qT=qT,
+        p=p
+    )
+    print("g = {}".format(resultG), "\n")
 
 
 def main():
-    initMatrix()
+    initialConditions()
 
 
 if __name__ == '__main__':
