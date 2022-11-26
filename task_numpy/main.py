6347d4d9a97124d7d57190519b7d9dcea2399288
sanchoz <alexvaa2010@mail.ru>
2022-11-27 00:38:43 +0300

    Рефакторинг: удаление рекурсий и вычисление всех значений через слайсы и методами numpy

--- task_numpy/main.py
+++ task_numpy/main.py
@@ -3,42 +3,17 @@ import numpy as np
 countN = 85
 
 
-def initMatrix():
-    matrixM_diag = [i * 2 + 1 for i in range(1, countN + 1)]
+def initMatrix_v2():
+    matrixM_diag = np.arange(3, 171 + 1, 2)
     matrixM = np.diag(np.array(matrixM_diag))
     matrixM[(matrixM == 0)] = 2
 
-    matrixN = np.zeros((countN, countN))
-    matrixN_row_even = [4, 4, 5]
-    matrixN_row_odd = [4, 7]
-    for indexR, row in enumerate(matrixN):
-        for indexC, column in enumerate(row):
-            # Четная(even) строка
-            if indexR % 2 == 0:
-                pos = indexC % len(matrixN_row_even)
-                matrixN[indexR, indexC] = matrixN_row_even[pos]
+    matrixN = np.zeros([countN, countN])
+    matrixN[(matrixN == 0)] = 4
+    matrixN[::2, 2::3] = 5
+    matrixN[1::2, 1::2] = 7
 
-            # Нечетная(odd) строка
-            else:
-                pos = indexC % len(matrixN_row_odd)
-                matrixN[indexR, indexC] = matrixN_row_odd[pos]
     return matrixM, matrixN
-
-
-def pSet(arr, i):
-    if i < countN:
-        arr = np.append(arr, 1 + np.random.normal(2, 4))
-        i += 1
-        arr = pSet(arr, i)
-    return arr
-
-
-def qSet(arr, j, matrixN):
-    if j < countN:
-        arr = np.append(arr, np.multiply(matrixN[24, j], matrixN[71, j]))
-        j += 1
-        arr = qSet(arr, j, matrixN)
-    return arr
 
 
 def findF(q, qT, p):
@@ -50,50 +25,48 @@ def findF(q, qT, p):
     return result
 
 
-def sumG(matrixM, matrixN, multQ, i, arr):
-    if i < countN:
-        _sum = matrixM[i, i] - matrixN[i, i] + multQ
-
-        arr = np.append(arr, _sum)
-        i += 1
-        arr = sumG(matrixM, matrixN, multQ, i, arr)
-    return arr
-
-
 def findG(matrixM, matrixN, q, qT, p):
     multQ = np.dot(qT, q)
 
-    _set = sumG(matrixM, matrixN, multQ, 0, np.array([]))
+    diagM = np.diag(matrixM)
+    diagN = np.diag(matrixN)
 
-    result = np.linalg.norm(_set, 2)
+    sub = diagM - diagN
+
+    _sum = np.add(sub, multQ)
+
+    result = np.linalg.norm(_sum, 2)
 
     return result
 
 
 def initialConditions():
-    M, N = initMatrix()
-    print("M = {}".format(M), "\n")
-    print("N = {}".format(N), "\n")
+    M, N = initMatrix_v2()
 
-    p = pSet(arr=np.array([]), i=0)
-    print("p = {}".format(p), "\n")
+    p = np.full(countN, 1) + np.random.normal(2, 4, size=85)
+    print("p: {}".format(p), "\n")
 
-    qT = qSet(arr=np.array([]), j=0, matrixN=N)[np.newaxis]
-    print("qT = {}".format(qT), "\n")
+    qT = np.multiply(N[24], N[71])[np.newaxis]
+    print("qT: {}".format(qT), "\n")
+
     q = qT.T
-    print("q = {}".format(q), "\n")
+    print("q: {}".format(q), "\n")
 
-    resultF = findF(q, qT, p)
-    print("f = {}".format(resultF), "\n")
+    f = findF(
+        q=q,
+        qT=qT,
+        p=p
+    )
+    print("f: {}".format(f), "\n")
 
-    resultG = findG(
+    g = findG(
         matrixM=M,
         matrixN=N,
         q=q,
         qT=qT,
         p=p
     )
-    print("g = {}".format(resultG), "\n")
+    print("g: {}".format(g), "\n")
 
 
 def main():
